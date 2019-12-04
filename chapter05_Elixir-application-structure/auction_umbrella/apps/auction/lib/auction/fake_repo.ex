b7d6cea787c7c7a641a813eaf844b39f8dc4dbaa
Pauli Marcello <m.pauli@softfair.de>
2019-12-04 16:04:29 +0100

    finished 5.2.3

--- chapter05_Elixir-application-structure/auction_umbrella/apps/auction/lib/auction/fake_repo.ex
+++ chapter05_Elixir-application-structure/auction_umbrella/apps/auction/lib/auction/fake_repo.ex
@@ -1,27 +1,3 @@
-defmodule Auction.Item do
-  defstruct [:id, :title, :description, :ends_at]
-end
-
-
-# this module helps retrieving data from the database
-defmodule Auction do
-  alias Auction.{FakeRepo, Item}
-
-  @repo FakeRepo
-
-  def list_items() do
-    @repo.all(Item)
-  end
-
-  def get_item(id) do
-    @repo.get!(Item, id)
-  end
-
-  def get_item_by(attrs) do
-    @repo.get_by(Item, attrs)
-  end
-end
-
 defmodule Auction.FakeRepo do
   alias Auction.{Item}
 
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
+### load file into iex
+iex -r filename
+
+### recompile from iex session
+c "filename"
+
 ### show all created routes
 mix phx.routes
